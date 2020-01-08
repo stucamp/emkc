c860f63fd6f8ce69f3a3a4dcb0f40b7eadfbd05d
Stu Campbell <stucampbell.git@gmail.com>
2020-01-07 20:09:44 -0700

    tweak

--- Go/fibonnaci_Seq/main.go
+++ Go/fibonnaci_Seq/main.go
@@ -11,13 +11,13 @@ var PHI float64 = (math.Sqrt(5) + 1) / 2
 var phi float64 = (-math.Sqrt(5) + 1) / 2
 
 func main() {
-    value1, _ := strconv.Atoi(os.Args[1])
-    value2, _ := strconv.Atoi(os.Args[2])
+	value1, _ := strconv.Atoi(os.Args[1])
+	value2, _ := strconv.Atoi(os.Args[2])
 
 	results := findNext(value1, value2)
 
 	for i, fib := range results {
-		if i==0 {
+		if i == 0 {
 			fmt.Printf("%d", fib)
 		} else {
 			fmt.Printf(",%d", fib)
@@ -28,7 +28,7 @@ func main() {
 func findNext(start int, n int) []int {
 	beg := findIndex(start)
 	var results []int
-	for i := beg+1; i <= n+beg ; i++ {
+	for i := beg + 1; i <= n+beg; i++ {
 		results = append(results, fib(i))
 	}
 	return results
@@ -36,12 +36,12 @@ func findNext(start int, n int) []int {
 
 func findIndex(n int) int {
 	eps := math.Pow(10, -10)
-	one := float64(n) * math.Sqrt(5) + eps
+	one := float64(n)*math.Sqrt(5) + eps
 	almost := math.Log(one) / math.Log(PHI)
 	return int(math.Round(almost))
 }
 
 func fib(n int) int {
-	almost := (math.Pow(PHI,float64(n)) - math.Pow(phi,float64(n))) / math.Sqrt(5)
+	almost := (math.Pow(PHI, float64(n)) - math.Pow(phi, float64(n))) / math.Sqrt(5)
 	return int(math.Round(almost))
 }
